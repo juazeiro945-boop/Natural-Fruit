3201206a7e26cac3c1f3d59b50db6836546c8d76
juazeiro945-boop <juazeiro945@gmail.com>
2025-11-21 21:33:54 -0300

    Update DashboardVendedor.vue

--- src/views/DashboardVendedor.vue
+++ src/views/DashboardVendedor.vue
@@ -968,7 +968,7 @@ const generateReceipt = async (pedido) => {
   doc.setFontSize(9)
   doc.text('CNPJ: 60.127.371/0001-60', 15, 56)
   doc.text('Juazeiro, Bahia, Brasil', 15, 61)
-  doc.text('Telefone: (74) 99999-9999', 15, 66)
+  doc.text('Telefone: (87) 98864-1590', 15, 66)
   
   doc.setFontSize(11)
   doc.setFont('helvetica', 'bold')
